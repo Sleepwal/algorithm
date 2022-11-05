3399995d6c53a1e6c668f0ff5a8cd756690824d4
SleepWalker <SleepWalker@123.com>
2022-11-05 10:59:30 +0800

    左神代码

---
+++ src/class05/Code01_PreInPosTraversal.java
@@ -0,0 +1,180 @@
+package class05;
+
+import java.util.Stack;
+
+public class Code01_PreInPosTraversal {
+
+	public static class Node {
+		public int value;
+		public Node left;
+		public Node right;
+
+		public Node(int data) {
+			this.value = data;
+		}
+	}
+
+	public static void Recur(Node head) {
+		//第一次进入该函数
+		if (head == null) {
+			return;
+		}
+		//第一次进入该函数, 此处为先序, 左右节点都没有访问过
+
+		preOrderRecur(head.left);
+		//第二次进入该函数, 先执行完preOrderRecur函数, 再返回到该函数
+		//此处为中序遍历, 已经访问完左节点, 还未访问右节点
+
+		preOrderRecur(head.right);
+		//第三次进入该函数
+		//此处为后序遍历, 左右节点都访问完了
+	}
+
+	public static void preOrderRecur(Node head) {
+		if (head == null) {
+			return;
+		}
+		System.out.print(head.value + " ");
+		preOrderRecur(head.left);
+		preOrderRecur(head.right);
+	}
+
+	public static void inOrderRecur(Node head) {
+		if (head == null) {
+			return;
+		}
+		inOrderRecur(head.left);
+		System.out.print(head.value + " ");
+		inOrderRecur(head.right);
+	}
+
+	public static void posOrderRecur(Node head) {
+		if (head == null) {
+			return;
+		}
+		posOrderRecur(head.left);
+		posOrderRecur(head.right);
+		System.out.print(head.value + " ");
+	}
+
+	public static void preOrderUnRecur(Node head) {
+		System.out.print("pre-order: ");
+		if (head != null) {
+			Stack<Node> stack = new Stack<Node>();
+			//1.头节点先进栈
+			stack.add(head);
+			while (!stack.isEmpty()) {
+				//2.弹出一个节点
+				head = stack.pop();
+				//3.打印该节点
+				System.out.print(head.value + " ");
+				//4.先右节点进栈
+				if (head.right != null) {
+					stack.push(head.right);
+				}
+				//4.再左节点进栈
+				if (head.left != null) {
+					stack.push(head.left);
+				}
+			} //5.直到栈为空
+		}
+		System.out.println();
+	}
+
+	public static void inOrderUnRecur(Node head) {
+		System.out.print("in-order: ");
+		if (head != null) {
+			Stack<Node> stack = new Stack<Node>();
+			while (!stack.isEmpty() || head != null) {
+				if (head != null) {
+					stack.push(head);
+					head = head.left;
+				} else {
+					head = stack.pop();
+					System.out.print(head.value + " ");
+					head = head.right;
+				}
+			}
+		}
+		System.out.println();
+	}
+
+	public static void posOrderUnRecur1(Node head) {
+		System.out.print("pos-order: ");
+		if (head != null) {
+			Stack<Node> s1 = new Stack<Node>();
+			Stack<Node> s2 = new Stack<Node>();
+			s1.push(head);
+			while (!s1.isEmpty()) {
+				head = s1.pop();
+				s2.push(head);
+				if (head.left != null) {
+					s1.push(head.left);
+				}
+				if (head.right != null) {
+					s1.push(head.right);
+				}
+			}
+			while (!s2.isEmpty()) {
+				System.out.print(s2.pop().value + " ");
+			}
+		}
+		System.out.println();
+	}
+
+	public static void posOrderUnRecur2(Node h) {
+		System.out.print("pos-order: ");
+		if (h != null) {
+			Stack<Node> stack = new Stack<Node>();
+			stack.push(h);
+			Node c = null;
+			while (!stack.isEmpty()) {
+				c = stack.peek();
+				if (c.left != null && h != c.left && h != c.right) {
+					stack.push(c.left);
+				} else if (c.right != null && h != c.right) {
+					stack.push(c.right);
+				} else {
+					System.out.print(stack.pop().value + " ");
+					h = c;
+				}
+			}
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		Node head = new Node(5);
+		head.left = new Node(3);
+		head.right = new Node(8);
+		head.left.left = new Node(2);
+		head.left.right = new Node(4);
+		head.left.left.left = new Node(1);
+		head.right.left = new Node(7);
+		head.right.left.left = new Node(6);
+		head.right.right = new Node(10);
+		head.right.right.left = new Node(9);
+		head.right.right.right = new Node(11);
+
+		// recursive
+		System.out.println("==============recursive==============");
+		System.out.print("pre-order: ");
+		preOrderRecur(head);
+		System.out.println();
+		System.out.print("in-order: ");
+		inOrderRecur(head);
+		System.out.println();
+		System.out.print("pos-order: ");
+		posOrderRecur(head);
+		System.out.println();
+
+		// unrecursive
+		System.out.println("============unrecursive=============");
+		preOrderUnRecur(head);
+		inOrderUnRecur(head);
+		posOrderUnRecur1(head);
+		posOrderUnRecur2(head);
+
+	}
+
+}
