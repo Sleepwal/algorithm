4d681e39ca3f42c6fa2d08a26214d907b8f56e8a
SleepWalker <SleepWalker@123.com>
2022-11-05 23:59:41 +0800

    CBT add 注释

--- src/class05/Code05_IsCBT.java
+++ src/class05/Code05_IsCBT.java
@@ -19,17 +19,21 @@ public class Code05_IsCBT {
 			return true;
 		}
 		LinkedList<Node> queue = new LinkedList<>();
-		boolean leaf = false;
+		boolean leaf = false; //是否为叶子结点
 		Node l = null;
 		Node r = null;
 		queue.add(head);
+
 		while (!queue.isEmpty()) {
 			head = queue.poll();
 			l = head.left;
 			r = head.right;
+			
+			//遇到不双全的节点后，后面的节点应该为叶子节点，居然有孩子，不符合条件
 			if ((leaf && (l != null || r != null)) || (l == null && r != null)) {
 				return false;
 			}
+
 			if (l != null) {
 				queue.add(l);
 			}
